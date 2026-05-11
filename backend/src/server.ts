bff5c57f587ea5abc476f127c2de72453cf53724
puviyarasans <puviyarasansasi123@gmail.com>
2026-05-12 00:21:42 +0530

    / updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -28,7 +28,7 @@ app.use(cors({
         "http://localhost:3000",
         "http://localhost:5173",
         "https://inventory-silk-kappa.vercel.app",
-        "https://inventory-ilai.vercel.app/"
+        "https://inventory-ilai.vercel.app"
     ]
 }));
 
